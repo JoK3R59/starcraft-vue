ae1dbef282ba89ee9170d8e8e9a5a78a910e31bd
JoK3R59 <51001366+JoK3R59@users.noreply.github.com>
2021-04-15 11:47:13 +0200

    Regles
    Inclus page Règles avec CSS : à terminer

--- src/components/Regles/Regles.vue
+++ src/components/Regles/Regles.vue
@@ -1,13 +1,63 @@
 <template>
-    <p>Salut regles !</p>
+
+    <div class="container">
+
+        <div class="Order">
+
+            <h1 class="container TIPS title">
+                Référence d'Ordre
+            </h1>            
+            
+            <div :class="`Order-list list-${ i }`"
+             v-for="(item, i) in build"
+             v-bind:key="i">
+                
+                <div class="top">
+                    <img src="" alt=""/>
+                    <div>
+                        <h2 class="">
+                            {{ item.name }}
+                        </h2>
+                        <p>
+                            {{ item.recap.a }}
+                        </p>
+                        <p>
+                            {{ item.recap.b }}
+                        </p>
+                        <p v-if="item.recap.c">
+                            {{ item.recap.c }}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="bot">
+                    <img src="" alt=""/>
+                    <p>
+                        {{ item.recap.spe }}
+                    </p>
+                </div>
+            </div>
+
+            <button>
+                Plus d'informations
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
+import build from '../../data/Order/build';
+
 export default {
-    name: 'Regles'
+    name: 'Regles',
+    data() {
+        return {
+            build
+        }
+    },
 }
 </script>
 
-<style>
+<style src="./Regles.css" scoped>
 
 </style>
