01f6b6b58afb3d282e2483e4eafeec81035ca08c
JoK3R59 <51001366+JoK3R59@users.noreply.github.com>
2021-04-16 21:51:40 +0200

    order MAJ
    Ajout du recap' Order + CSS
    MAJ du Data Order->Build

---
+++ src/components/Regles/Order/Order.vue
@@ -0,0 +1,74 @@
+<template>
+
+    <div class="container">
+
+        <div class="Order">
+
+            <h1 class="container TIPS title">
+                Référence d'Ordre
+            </h1>            
+            
+            <div :class="`Order-list list-${ i }`"
+             v-for="(item, i) in build"
+             v-bind:key="i">
+                
+                <div class="top">
+
+                    <img class="top-img"
+                        :src="getImgUrl(item.img)" alt=""/>
+                    
+                    <div>
+                        <h2 class="">
+                            {{ item.name }}
+                        </h2>
+                        <p>
+                            {{ item.recap.a }}
+                        </p>
+                        <p>
+                            {{ item.recap.b }}
+                        </p>
+                        <p v-if="item.recap.c">
+                            {{ item.recap.c }}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="bot">
+                    <img class="bot-img"
+                     :src="getImgUrl(item.imgSpe)" alt=""/>
+                    <p>
+                        {{ item.recap.spe }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="button">
+                <button class="btn btn-info">
+                    Plus d'informations...
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import build from '../../../data/Order/build';
+
+export default {
+    name: 'Order',
+    data() {
+        return {
+            build
+        }
+    },
+    methods: {
+        getImgUrl(pic) {
+            return require('../../../assets/Images/Interfaces/' + pic + '.png')
+        },
+    }
+}
+</script>
+
+<style src="./Order.css" scoped>
+
+</style>
--- src/components/Regles/Regles.vue
+++ src/components/Regles/Regles.vue
@@ -1,60 +1,21 @@
 <template>
 
-    <div class="container">
-
-        <div class="Order">
-
-            <h1 class="container TIPS title">
-                Référence d'Ordre
-            </h1>            
-            
-            <div :class="`Order-list list-${ i }`"
-             v-for="(item, i) in build"
-             v-bind:key="i">
-                
-                <div class="top">
-                    <img src="" alt=""/>
-                    <div>
-                        <h2 class="">
-                            {{ item.name }}
-                        </h2>
-                        <p>
-                            {{ item.recap.a }}
-                        </p>
-                        <p>
-                            {{ item.recap.b }}
-                        </p>
-                        <p v-if="item.recap.c">
-                            {{ item.recap.c }}
-                        </p>
-                    </div>
-                </div>
-
-                <div class="bot">
-                    <img src="" alt=""/>
-                    <p>
-                        {{ item.recap.spe }}
-                    </p>
-                </div>
-            </div>
-
-            <button>
-                Plus d'informations
-            </button>
-        </div>
+    <div class="rules">
+        
+        <order></order>
     </div>
+
 </template>
 
 <script>
-import build from '../../data/Order/build';
+import Order from './Order/Order';
 
 export default {
     name: 'Regles',
-    data() {
-        return {
-            build
-        }
+    components: {
+        'order' : Order,
     },
+    
 }
 </script>
 
